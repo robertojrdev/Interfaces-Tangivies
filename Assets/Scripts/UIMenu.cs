3201e6cef9098b2b3d159feee4f519ae55bdcbc4
tehdevv <41214812+tehdevv@users.noreply.github.com>
2018-12-11 13:50:08 +0000

    Fixed commits

---
+++ Assets/Scripts/UIMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIMenu : MonoBehaviour 
+{
+    public Button playBtn;
+    public Button settingsBtn;
+    public Button exitBtn;
+
+    private void Start()
+    {
+        playBtn.onClick.AddListener(Play);
+        settingsBtn.onClick.AddListener(ShowSettings);
+        exitBtn.onClick.AddListener(Application.Quit);
+    }
+
+    private void ShowSettings()
+    {
+
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
