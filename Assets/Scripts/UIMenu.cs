44686bd4b87e6a4173ab65ae226ccd65b256b139
tehdevv <41214812+tehdevv@users.noreply.github.com>
2019-02-04 05:30:00 +0000

    Credits

--- Assets/Scripts/UIMenu.cs
+++ Assets/Scripts/UIMenu.cs
@@ -8,17 +8,28 @@ public class UIMenu : MonoBehaviour
     public Button playBtn;
     public Button settingsBtn;
     public Button exitBtn;
+    public Button creditsBtn;
+    public Button closeCreditsBtn;
+
+    public GameObject credits;
 
     private void Start()
     {
         playBtn.onClick.AddListener(Play);
         settingsBtn.onClick.AddListener(ShowSettings);
         exitBtn.onClick.AddListener(Application.Quit);
+        creditsBtn.onClick.AddListener(ToggleCredits);
+        closeCreditsBtn.onClick.AddListener(ToggleCredits);
     }
 
     private void ShowSettings()
     {
 
+    }
+
+    private void ToggleCredits()
+    {
+        credits.SetActive(!credits.activeInHierarchy);
     }
 
     void Play()
