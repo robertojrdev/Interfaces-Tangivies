3830204e5063fb5d85b44a5474566fdb18c28984
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-02-04 05:01:41 +0000

    videos, tutorial pronto

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Tutorial : MonoBehaviour
+{
+    public VideoPlayer player;
+    public Animator anim;
+
+    public void ShowVideo(VideoClip video)
+    {
+        player.clip = video;
+        player.time = 0;
+        player.Play();
+        anim.SetBool("Visible", true);
+    }
+
+    public void Stop()
+    {
+        player.Stop();
+        anim.SetBool("Visible", false);
+
+    }
+}
